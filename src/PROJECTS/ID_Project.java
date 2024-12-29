984cb1bb5af487f85dac8af34ea34311fe68b4d4
Yair Siboni <yairsib@edu.aac.ac.il>
2024-12-29 23:43:01 +0200

    a

--- src/PROJECTS/ID_Project.java
+++ src/PROJECTS/ID_Project.java
@@ -1,5 +1,6 @@
 package PROJECTS;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class ID_Project {
@@ -76,6 +77,66 @@ public class ID_Project {
         if (verification(arrayID , verification) == true){
             System.out.println(checkDigit(verification));
         }
+
+    }
+}
+
+class Wordle_Exe {
+
+    public static boolean checkLetters ( String playerWord ){
+        if (playerWord.length() != 5){
+            return false;
+        }
+        return true;
+    }
+
+    public static String checkGuesses ( String theWord , String playerWord){
+        String playerWordLower = playerWord.toLowerCase();
+        String newString = "";
+        for (int i = 0; i < theWord.length(); i++) {
+            if (theWord.charAt(i) == playerWordLower.charAt(i)){
+                newString += "G";
+                continue;
+            }
+            String answer = "_";
+            for (int j = 0; j < theWord.length(); j++) {
+                if (playerWordLower.charAt(i) == theWord.charAt(j)){
+                    answer = "Y";
+                    break;
+                }
+            }
+            newString += answer;
+        }
+        return newString;
+    }
+
+    public static void main(String[] args) {
+        String [] words = {"apple" , "house" , "beach" , "table" , "chair" , "sweet" , "water" , "dance" , "movie" , "bread"};
+        Random random = new Random();
+        int index = random.nextInt(words.length);
+        String theWord = words[index];
+        System.out.println("Hello , Lets play!!! , Try to guess my word...");
+        Scanner scanner = new Scanner(System.in);
+        String playerWord = scanner.next();
+        int turns = 6;
+        String answer = "";
+        while (turns != 0){
+            if (checkLetters(playerWord)){
+                answer = checkGuesses(theWord , playerWord);
+                turns --;
+                System.out.println(answer);
+            }
+            if (answer.equals("GGGGG")){
+                System.out.println("you won!!!");
+                break;
+            } else {
+                System.out.println("you've left " + turns + " turns ,try another word");
+                playerWord = scanner.next();
+
+            }
+
+        }
+
 
     }
 }
